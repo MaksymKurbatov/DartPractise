51f3f4f991a66dcb06cb9460808604afe71ea66f
Maksym_Kurbatov <Maksym_Kurbatov@epam.com>
2023-06-19 17:42:34 +0300

    Add API calls

--- crypto_coins_list/lib/features/crypto_list/view/crypto_list_screen.dart
+++ crypto_coins_list/lib/features/crypto_list/view/crypto_list_screen.dart
@@ -1,5 +1,8 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins_repository.dart';
+
+import '../widgets/crypto_coin_tile.dart';
 
 class CryptoListScreen extends StatefulWidget {
   const CryptoListScreen({super.key, required this.title});
@@ -11,51 +14,33 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
+  List<CryptoCoin>? _cryptoCoinsList;
+
+  @override
+  void initState() {
+    loadCryptoCoins();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: ListView.separated(
-        itemCount: 10,
-        separatorBuilder: (context, index) => const Divider(),
-        itemBuilder: (context, i) {
-          const coinName = "BNB";
-          return const CryptoCoinTile(coinName: coinName);
-        },
-      ),
+      body: (_cryptoCoinsList == null)
+          ? const SizedBox()
+          : ListView.separated(
+              itemCount: _cryptoCoinsList!.length,
+              separatorBuilder: (context, index) => const Divider(),
+              itemBuilder: (context, i) {
+                final coinName = _cryptoCoinsList![i];
+                return CryptoCoinTile(coin: coinName);
+              },
+            ),
     );
   }
-}
 
-class CryptoCoinTile extends StatelessWidget {
-  const CryptoCoinTile({
-    super.key,
-    required this.coinName,
-  });
-
-  final String coinName;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(
-        'assets/svg/bitcoin_logo.svg',
-        width: 30,
-        height: 30,
-      ),
-      trailing: const Icon(Icons.arrow_forward_ios),
-      title: Text(
-        coinName,
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
-      subtitle: Text(
-        "7000000",
-        style: Theme.of(context).textTheme.labelSmall,
-      ),
-      onTap: () {
-        Navigator.of(context).pushNamed('/coins', arguments: coinName);
-        //     MaterialPageRoute(builder: (context) => const CoinScreen()));
-      },
-    );
+  Future<void> loadCryptoCoins() async {
+    _cryptoCoinsList = await CryproCoinsRepository().getCoinsList();
+    setState(() {});
   }
 }
--- crypto_coins_list/lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ crypto_coins_list/lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,33 +1,30 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CryptoCoinTile extends StatelessWidget {
   const CryptoCoinTile({
     super.key,
-    required this.coinName,
+    required this.coin,
   });
 
-  final String coinName;
+  final CryptoCoin coin;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: SvgPicture.asset(
-        'assets/svg/bitcoin_logo.svg',
-        width: 30,
-        height: 30,
-      ),
+      leading: Image.network(coin.imageURL),
       trailing: const Icon(Icons.arrow_forward_ios),
       title: Text(
-        coinName,
+        coin.name,
         style: Theme.of(context).textTheme.bodyMedium,
       ),
       subtitle: Text(
-        "5000000",
+        '${coin.priceInUSD}\$',
         style: Theme.of(context).textTheme.labelSmall,
       ),
       onTap: () {
-        Navigator.of(context).pushNamed('/coins', arguments: coinName);
+        Navigator.of(context).pushNamed('/coins', arguments: coin);
         //     MaterialPageRoute(builder: (context) => const CoinScreen()));
       },
     );
