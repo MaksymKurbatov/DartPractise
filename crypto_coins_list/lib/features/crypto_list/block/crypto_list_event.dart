226bcf718be9128040f4d0dd730b00c6aae151d7
Maksym_Kurbatov <Maksym_Kurbatov@epam.com>
2023-06-21 17:31:56 +0300

    fix bug

--- crypto_coins_list/lib/features/crypto_list/block/crypto_list_event.dart
+++ crypto_coins_list/lib/features/crypto_list/block/crypto_list_event.dart
@@ -1,16 +1,14 @@
 part of 'crypto_list_bloc.dart';
 
-class CryptoListEvent extends Equatable {6
+class CryptoListEvent extends Equatable {
   @override
-  // TODO: implement props
-  List<Object?> get props =>[];
+  List<Object?> get props => [];
 }
 
 class LoadCryprtoList extends CryptoListEvent {
   LoadCryprtoList({this.completer});
   final Completer? completer;
 
-    @override
-
-  List<Object?> get props =>[completer];
+  @override
+  List<Object?> get props => [completer];
 }
